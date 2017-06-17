dbe40873c4f7285488e07afdb309488b2ffd6e28
Min Borin <minborin.gitlab@gmail.com>
2017-06-18 03:05:25 +0700

    add first project

---
+++ src/main/java/com/example/demo/B008MinBorinSpringMiniProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B008MinBorinSpringMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B008MinBorinSpringMiniProjectApplication.class, args);
+	}
+}
