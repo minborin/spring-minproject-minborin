09a382675590fe52bcf55832ec9aa15d4b791a62
Min Borin <minborin.gitlab@gmail.com>
2017-06-18 18:54:58 +0700

    form post

--- src/main/java/spring/min/project/language/StudentController.java
+++ src/main/java/spring/min/project/language/StudentController.java
@@ -1,13 +1,22 @@
 package spring.min.project.language;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class StudentController {
-		@RequestMapping("student")
-		public String studentUC(){
+		@GetMapping("student")
+		public String studentUC(ModelMap mp){
+			mp.addAttribute("STUDENT",new Student());
 			return "student-cu";				
+		}
+		@PostMapping("student")
+		@ResponseBody
+		public Student result(@ModelAttribute Student stu){
+			return stu	;
 		}
 }
